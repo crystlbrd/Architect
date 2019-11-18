49d5f1ab14cc6d0a943ab61b438e79b47488a7b1
Emanuel Bitkov <emanuel.bitkov@regenta-verlag.de>
2019-11-18 10:49:23 +0100

    debugging

--- src/Blueprints/ConnectorEntity.php
+++ src/Blueprints/ConnectorEntity.php
@@ -269,7 +269,7 @@ abstract class ConnectorEntity extends Entity implements Iterator, Countable
         foreach ($this->List as $item) {
             $entity = $item['model'];
             foreach ($properties as $prop) {
-                if (isset($entity->$prop)) {
+                if ($entity->$prop != null) {
                     if (count($properties) == 1 && $flat) {
                         $items[] = $entity[$prop];
                     } else {
